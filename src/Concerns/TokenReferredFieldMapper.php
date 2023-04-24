6776e2745bfb4bbb60479ad7e9eee8f9083726e5
Anthony Duhaime <aduhaime@armcanada.ca>
2023-04-24 12:35:06 -0400

    Optimize TokenReferredFieldMapper

--- src/Concerns/TokenReferredFieldMapper.php
+++ src/Concerns/TokenReferredFieldMapper.php
@@ -4,19 +4,27 @@ namespace Armcanada\LaravelReferredField\Concerns;
 
 use Armcanada\LaravelReferredField\Models\ReferredField;
 use ArrayAccess;
+use Illuminate\Database\Eloquent\Collection;
 
 /**
  * Maps token to referred field
  */
 class TokenReferredFieldMapper
 {
+    private Collection $referredFields;
+
+    public function __construct()
+    {
+        $this->referredFields = (config('laravel-referred-field.referred_field_class', Armcanada\LaravelReferredField\Models\ReferredField::class))::all();    
+    }
+
     public function mapTokensToReferredFields(array $tokens) : ArrayAccess
     {
         return collect($tokens)->mapWithKeys(function($token) {
-            $referredField = config('laravel-referred-field.referred_field_class', Armcanada\LaravelReferredField\Models\ReferredField::class)::findByLabel($token);
+            $referredField = $this->referredFields->firstWhere('label', $token);
 
             return [
-                $token => $referredField
+                $token => $referredField,
             ];
         });
     }
